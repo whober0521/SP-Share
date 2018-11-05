e29dcfd28a41e32cd2a451cc193c0108705b430a
Jenny Chen <whober0521@gmail.com>
2018-11-04 20:10:23 -0600

    About

--- SP Share/Controllers/HomeController.cs
+++ SP Share/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SP_Share.Controllers
 {
@@ -15,14 +11,7 @@ namespace SP_Share.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = " CSE 5382 Secure Programming Fall 2018 Assignment:  SP Share";
 
             return View();
         }
