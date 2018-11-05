91f3d389866bb6d3fd88589f653aff041387c615
Jenny Chen <whober0521@gmail.com>
2018-11-05 15:11:20 -0600

    Group

--- SP Share/Models/SPContext.cs
+++ SP Share/Models/SPContext.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using System.Data.Entity;
 
@@ -7,6 +8,7 @@ namespace SP_Share.Models
     public class SPContext : DbContext
     {
         public DbSet<User> User { get; set; }
+        public DbSet<Group> Group { get; set; }
 
         public SPContext()
             : base("name=DefaultConnection")
@@ -30,7 +32,7 @@ namespace SP_Share.Models
         public string Name { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "{0} length at least {2} characters", MinimumLength = 4)]
+        [StringLength(200, ErrorMessage = "{0} length at least {2} characters", MinimumLength = 4)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
@@ -42,25 +44,13 @@ namespace SP_Share.Models
         public bool IsAdmin { get; set; }
     }
 
-    //public class Groupa
-    //{
-    //    [Key, Column(Order = 0)]
-    //    [Required]
-    //    [DisplayName("公司代碼")]
-    //    [StringLength(2)]
-    //    [MaxLength(2, ErrorMessage = "長度不可超過 2")]
-    //    public string Company { get; set; }
+    public class Group
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Idx { get; set; }
 
-    //    [Key, Column(Order = 1)]
-    //    [Required]
-    //    [DisplayName("組別")]
-    //    [StringLength(1)]
-    //    [MaxLength(1, ErrorMessage = "長度不可超過 1")]
-    //    public string Code { get; set; }
-
-    //    [DisplayName("對應編號")]
-    //    [StringLength(2)]
-    //    [MaxLength(2, ErrorMessage = "長度不可超過 2")]
-    //    public string SerialNo { get; set; }
-    //}
+        [StringLength(10)]
+        public string Name { get; set; }
+    }
 }
